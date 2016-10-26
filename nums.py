ba62ce2b30f5fbb5d79dbfb8a32577eaaa360a36
RI45816 <uu3@umbc.edu>
2016-10-26 16:41:34 -0400

    @D5EW2@

--- nums.py
+++ nums.py
@@ -1,9 +1,9 @@
 # File:    nums.py
 # Started: by Dr. Gibson
-# Author:  YOUR NAME GOES EHRE
-# Date:    DATE GOES HERE
-# Section: SECTION NUMBER GOES HERE
-# E-mail:  EMAIL_GOES_HERE@umbc.edu 
+# Author:  Uzoma Uwanamodo
+# Date:    10/26/2016
+# Section: 05
+# E-mail:  uu3@umbc.edu
 # Description:
 #   This file contains python code that uses functions to 
 #   allow a user to get basic information about a number
@@ -19,7 +19,8 @@ def evenOrOdd(number):
     ####################################################################
     # your function to determine if the number's even or odd goes here #
     ####################################################################
-
+    # return "even" if the remainder of the number divided by 2 is even, otherwise return "odd"
+    return ["even","odd"][number % 2]
 
 # posNegZero() takes in a number and returns its sign
 # Input:      num, an integer
@@ -29,7 +30,8 @@ def posNegZero(num):
     ##########################################################
     # your function to determine the number's sign goes here #
     ##########################################################
-
+    # return "negative" if x is less than 0, "postive" if x is greater than 0, otherwise return 0
+    return "negative" if num < 0 else "postive" if num > 0 else "zero"
 
 # getValidInt() takes in a minn and maxx, and gets a number from the
 #               user between those two numbers (inclusive)
@@ -44,7 +46,9 @@ def getValidInt(minn, maxx):
     # your code that loops until the user # 
     # enters a valid number goes here     #
     #######################################
-
+    while newInt not in range (minn, maxx):
+        newInt = int(input(message))
+    
     # while loop exited, return the user's choice
     return newInt
 
@@ -60,19 +64,19 @@ def main():
     # your code to call the function getValidInt() goes here #
     # ** use the constants MIN_VAL and MAX_VAL in your call  #
     ##########################################################
-
+    userNum = getValidInt(MIN_VAL, MAX_VAL)
 
     # call the posNegZero function and print out the result to the user
     #########################################################
     # your code to call the function posNegZero() goes here #
     #########################################################
-    print("The number", userNum, "is", sign)
+    print("The number", userNum, "is", posNegZero(userNum))
 
     # call the evenOrOdd function and print out the result to the user
     ########################################################
     # your code to call the function evenOrOdd() goes here #
     ########################################################
-    print("The number", userNum, "is", result)
+    print("The number", userNum, "is", evenOrOdd(userNum))
 
     # say goodbye and exit the program
     print("Thank you for using the number program!  Come again!")
